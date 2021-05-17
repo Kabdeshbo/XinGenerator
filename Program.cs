81d285dc54ff7cc3c71b0f1060b0bba74ba6b64c
Kabdeshbo <kabdeshbo@gmail.com>
2021-05-17 10:35:03 +0600

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XinGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GenerateXin());
+            Console.ReadKey();
+		}
+        static string GenerateXin()
+        {
+            Random rn = new Random();
+            string iin = "";
+            iin = DateTime.Now.ToString("yyMMdd") + rn.Next(1, 7) + rn.Next(1000, 9999);
+            iin = iin + GetTwelveNumber(iin);
+            return iin;
+        }
+        static int GetTwelveNumber(string text)
+        {
+            long[] array = new long[11];
+            long iin = Convert.ToInt64(text);
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                array[i] = iin % 10;
+                iin = iin / 10;
+            }
+            long razryad12 = 0;
+            long[] vesRazryada = new long[] { 3, 4, 5, 6, 7, 8, 9, 10, 11, 1, 2 };
+            for (int i = 0; i < array.Length; i++)
+            {
+                razryad12 = razryad12 + array[i] * (i + 1);
+            }
+            if (razryad12 % 11 == 10)
+            {
+                razryad12 = 0;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    razryad12 = razryad12 + array[i] * (vesRazryada[i]);
+                }
+            }
+            return (int)razryad12 % 11;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("XinGenerator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("XinGenerator")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("a2b9487e-0081-4d29-87e6-3396612bcac5")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
